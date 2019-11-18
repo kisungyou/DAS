f58e534d7c881c314d4ce226760a54ac2298a054
Kisung <ksyou496@gmail.com>
2019-11-17 21:48:56 -0500

    Rtsne added

--- R/ml_tsne.R
+++ R/ml_tsne.R
@@ -1,5 +1,4 @@
-#' Classical Multidimensional Scaling
-#' 
+#' t-SNE embedding
 #' 
 #' 
 #' 
@@ -12,51 +11,47 @@
 #' dat  = dat + matrix(rnorm(dat.n*dat.p, sd=0.1), ncol=dat.p)
 #' dmat = as.matrix(stats::dist(dat)) # distance matrix
 #' 
-#' ## run the algorithm
+#' ## run t-SNE and MDS for comparison
 #' iris.cmds = cmds(dmat, ndim=2)
+#' iris.tsne = tsne(dmat, ndim=2)
 #' 
 #' ## extract coordinates and class information
 #' cx = iris.cmds$embed # embedded coordinates of CMDS
+#' tx = iris.tsne$embed #                         t-SNE
 #' icol = iris[,5]      # class information
 #' 
+#' 
 #' ## visualize
-#' opar = par(pty="s")
-#' mc   = paste("CMDS with STRESS=",round(iris.cmds$stress,4),sep="")
+#' mc = paste("CMDS with STRESS=",round(iris.cmds$stress,4),sep="")
+#' mt = paste("tSNE with STRESS=",round(iris.tsne$stress,4),sep="")
+#' 
+#' opar <- par(mfrow=c(1,2), pty="s")
 #' plot(cx, col=icol,pch=19,main=mc)
+#' plot(tx, col=icol,pch=19,main=mt)
 #' par(opar)
 #' 
-#' @references 
-#' \insertRef{torgerson_multidimensional_1952}{DAS}
-#' 
-#' @author Kisung You
 #' @export
-cmds <- function(x, ndim=2){
+tsne <- function(x, ndim=2, ...){
   ##################################################3
   # Check Input and Transform
-  ndim = round(ndim)
   k  = as.integer(ndim)
   x  = check_input(x)
-  D2 = (x^2)          # now squared matrix
-  n  = nrow(D2)
   if ((length(ndim)>1)||(ndim<1)||(ndim>=nrow(x))){
     stop("* DAS::cmds - 'ndim' should be an integer in [1,nrow(x)). ")
   }
   
-  ##################################################3
-  # Computation
-  J = diag(n) - (1/n)*outer(rep(1,n),rep(1,n))
-  B = -0.5*J%*%D2%*%J
-  eigB = eigen(B)
+  ##################################################
+  # Pass to 'Rtsne'
+  dx  = stats::as.dist(x)
+  tmpout = Rtsne::Rtsne(dx, dims=k, ..., is_distance=TRUE)
+  Y   = tmpout$Y
+  DY  = as.matrix(stats::dist(Y))
   
-  LL = eigB$values[1:k]
-  EE = eigB$vectors[,1:k]
-  
-  # Y  = as.matrix(base::scale((EE%*%diag(sqrt(LL))), center=TRUE, scale=FALSE))
-  Y  = EE%*%diag(sqrt(LL))
-  DY = as.matrix(stats::dist(Y))
-  
+  ##################################################
+  # Return
   output = list()
   output$embed  = Y
   output$stress = compute_stress(x, DY)
   return(output)
+  
 }
--- R/package-DAS.R
+++ R/package-DAS.R
@@ -13,6 +13,7 @@
 #' @importFrom stats dist hclust ks.test ecdf
 #' @importFrom transport transport
 #' @importFrom energy eqdist.etest
+#' @importFrom Rtsne Rtsne
 #' @importFrom Rcpp evalCpp
 #' @useDynLib DAS
 NULL
